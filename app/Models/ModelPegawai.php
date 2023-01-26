6a2287542daa9d870bab893fa492850b653e7d31
Vilaintes <vilaintes@gmail.com>
2023-01-26 17:38:37 +0800

    project pertama

---
+++ app/Models/ModelPegawai.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModelPegawai extends Model
+{
+    protected $table = "pegawai";
+    protected $primaryKey = "id";
+    protected $allowedFields = ['nama', 'no_hp', 'bidang', 'alamat', 'jenis_kelamin'];
+
+    function cari($katakunci)
+    {
+
+        $builder = $this->table("pegawai");
+        $arr_katakunci = explode(" ", $katakunci);
+        for ($x = 0; $x < count($arr_katakunci); $x++) {
+            $builder->orLike('nama', $arr_katakunci[$x]);
+            $builder->orLike('no_hp', $arr_katakunci[$x]);
+            $builder->orLike('alamat', $arr_katakunci[$x]);
+            $builder->orLike('jenis_kelamin', $arr_katakunci[$x]);
+        }
+        return $builder;
+    }
+}
